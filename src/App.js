331ab87d3fa1f832c031b5673d55f72e650f548f
chisengachisulo <128135390+chisengachisulo@users.noreply.github.com>
2023-12-03 01:11:20 +0000

    Rounded the temperature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
-import "./App.css"; 
-import Weather from "./Weather"
-
+import "./App.css";
+import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello world</h1> 
         <Weather />
       </header>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export default function Weather() {
   function showWeather(response) {
-    alert(`the weather in london is ${response.data.main.temp} degrees`);
+    alert(`the weather in london is ${Math.round(response.data.main.temp)} degrees`);
   }
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=4c9b53e4f8f5eb00df5915bdca340605&units=metric`;
   axios.get(apiUrl).then(showWeather);
