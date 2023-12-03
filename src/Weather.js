59917df08e676b7d7c5d0536b24174b6c2646a60
chisengachisulo <128135390+chisengachisulo@users.noreply.github.com>
2023-12-03 01:08:20 +0000

    Added axios integration to show weather of a city

---
+++ src/Weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+import axios from "axios";
+
+export default function Weather() {
+  function showWeather(response) {
+    alert(`the weather in london is ${response.data.main.temp} degrees`);
+  }
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=4c9b53e4f8f5eb00df5915bdca340605&units=metric`;
+  axios.get(apiUrl).then(showWeather);
+  return <h2>Hello from weather!</h2>;
+}
