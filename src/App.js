0bc9f08b23d67f82594dc7700d39ad217ec85b34
chisengachisulo <128135390+chisengachisulo@users.noreply.github.com>
2023-12-04 21:32:37 +0000

    added github link

--- src/App.js
+++ src/App.js
@@ -4,9 +4,18 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <Weather />
-      </header>
+      <Weather />
+      <p>
+        {" "}
+        <a
+          href="https://github.com/chisengachisulo/weather-react"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Open-source code
+        </a>{" "}
+        by chisenga chisulo
+      </p>
     </div>
   );
 }
