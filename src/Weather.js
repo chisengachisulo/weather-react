e6757963a5374d04f95462b17e8a220edacc2c25
chisengachisulo <128135390+chisengachisulo@users.noreply.github.com>
2023-12-04 21:03:50 +0000

    moved my project from codesandbox to vscode

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import "./weather.css";
 
 export default function Weather() {
+  let [city, setCity] = useState("");
+  let [temperature, setTemperature] = useState(null);
+
   function showWeather(response) {
-    alert(`the weather in london is ${Math.round(response.data.main.temp)} degrees`);
+    setTemperature({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      humidity: response.data.main.humidity,
+      wind: response.data.wind.speed,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+    console.log(response.data);
   }
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=london&appid=4c9b53e4f8f5eb00df5915bdca340605&units=metric`;
-  axios.get(apiUrl).then(showWeather);
-  return <h2>Hello from weather!</h2>;
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=4c9b53e4f8f5eb00df5915bdca340605&units=metric`;
+    axios.get(url).then(showWeather);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="search"
+          placeholder="Enter a city..."
+          value={city}
+          onChange={updateCity}
+        />
+        <input type="submit" value="search" />
+      </form>
+      {temperature && (
+        <div>
+          <ul>
+            <li>Temperature: {Math.round(temperature.temperature)}°C</li>
+            <li>Description: {temperature.description}</li>
+            <li>Humidity: {temperature.humidity}%</li>
+            <li>Wind: {temperature.wind}km/h</li>
+            <li>
+              <img src={temperature.icon} alt="temp icon" />
+            </li>
+          </ul>
+        </div>
+      )}
+    </div>
+  );
 }
